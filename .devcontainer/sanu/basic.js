7a392758bda9faa1cd2e66b678eb73bd2492fbd2
sanujitmajhi <80968929+sanu2002@users.noreply.github.com>
2023-07-31 14:31:11 +0000

    datatypes

---
+++ .devcontainer/sanu/basic.js
@@ -0,0 +1,43 @@
+        //    DATA TYPES IN JS 
+        // 31/07/2023
+        // +++++++++++++++++++++++++++++++
+
+
+
+// console.log('hey my name is sanu')
+       
+
+// let a=2
+// let b=3
+// console.log(a+b)
+
+
+// 'use strict'; //treat all js as newer version 
+// alert("hello") we are using nodejs not browser thats why it is showing error
+// let alert='sanu'
+// console.log(3+3) console.log('sanu')
+// ----------------------------------------
+// console.log(3+3); console.log('sanu')
+// this is ccorrect but u failed in code redability
+// ___________________________________________
+
+// primitive datatypes
+// number 
+// BigInt
+// String=""
+// Boolean=True/false
+// null=standalone value
+// undefined=>
+// Symbol=unique
+
+// Object
+let a='sanu'
+// console.log(typeof a )
+console.log(typeof null)
+console.log(typeof undefined)
+
+
+
+
+
+
