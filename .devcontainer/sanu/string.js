c5188c0007786f1e12f34216a921066108e091ac
sanujitmajhi <80968929+sanu2002@users.noreply.github.com>
2023-08-02 10:53:37 +0000

    this is all about string

---
+++ .devcontainer/sanu/string.js
@@ -0,0 +1,29 @@
+     //in this section we are going to discuss about all the concrpt regarding string how you can manupulate string lets parctise some function on it//
+
+let name='sanu'
+let village=' budhapank'
+
+// console.log( name + ' from' + village)
+// console.log(`${name} from ${village}`)  //this is the the new way to concatinate the string in js
+
+// in python it has same prototype
+// f"{name} from {village}"
+
+// let sub1=' sanu'
+// console.log(typeof sub1)
+// console.log(sub1.concat('majhi'))
+
+// let sub =new String('sanu')
+// console.log(typeof sub)
+// console.log(sub.charAt(2))
+// console.log(sub.includes('kalu')) it will check  whaether  name is present or not 
+// console.log(sub.indexOf('s'))
+// console.log(sub.length)
+// console.log(sub.split())
+// console.log(sub)
+// console.log(sub.trimLeft())
+// console.log(sub.trim())
+// console.log(sub.trimEnd())
+// console.log(sub)
+// console.log(sub.toUpperCase())
+// console.log(Symbol(sub))
