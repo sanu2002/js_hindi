f5dd2a6d2f22eed4e40f247c70a70d62cab02b0d
sanujitmajhi <80968929+sanu2002@users.noreply.github.com>
2023-08-02 10:11:40 +0000

    this is all about stack and heap

---
+++ .devcontainer/sanu/stack.js
@@ -0,0 +1,32 @@
+// stack-primitive datatypes
+//Heap-non-primitive
+
+// so we can that on stack and heap there is little bit diffrence between these 2 memeory what is that lets go i will explain everrythig in details
+
+
+let first='sanu'
+
+let another=first
+
+another='kalu'
+console.log(first)
+
+console.log(another)
+
+
+let information={
+    name:'sanu',
+    email:'sanujitmajhigmail.com'
+}
+
+let change=information
+change.name='kalu'
+console.log(information)
+console.log(change)
+
+
+
+
+
+
+
