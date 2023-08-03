a37451d6c3474dabc46c838e350e41baa06bae4d
sanujitmajhi <80968929+sanu2002@users.noreply.github.com>
2023-08-03 11:36:23 +0000

    datetime frist commit

---
+++ .devcontainer/sanu/datetime.js
@@ -0,0 +1,15 @@
+console.log(Date)
+console.log(Date.UTC())
+
+let mydate=new Date('2012-12-01');
+console.log(mydate.toString())
+
+console.log(`the current time is ${mydate}`)
+
+
+// console.log(typeof Date.UTC)
+// console.log(Date.__proto__)
+
+
+// date is object not a function just like Math theree is some proposal regarding this
+
