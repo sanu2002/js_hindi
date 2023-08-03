b77a1e282bc67aac9a739c1235c815f19aee5f8b
sanujitmajhi <80968929+sanu2002@users.noreply.github.com>
2023-08-03 11:00:12 +0000

    update in Number

--- .devcontainer/sanu/numbersandmath.js
+++ .devcontainer/sanu/numbersandmath.js
@@ -7,9 +7,53 @@ console.log(Math)
 // console.log(Math.sqrt(2));
 // console.log(Math.floor(Math.random()*10));
 
-let max=10
-let min=20
-console.log(Math.floor(Math.random() * (max-min+1)) +min)
+// let max=10
+// let min=20
+// console.log(Math.floor(Math.random() * (max-min+1)) +min)
+
+// let ara=[2,3,45,5,6]
+// console.log(Math.min(ara))
+// console.log(typeof Infinity)
+// console.log(Number.isFinite(100))
+// console.log(Number.isSafeInteger(100))
+// console.log(Number.parseFloat(100.2343))
+
+
+// let data=12.55
+// console.log(Math.round(data))
+// console.log(Number.__proto__)
+
+
+// let a = new Number(12);
+
+constructor
+: 
+// ƒ Number()
+// ƒ toExponential()
+// ƒ toFixed()
+// ƒ toLocaleString()
+// ƒ toPrecision()
+// ƒ toString()
+// ƒ valueOf()
+
+// let datag=12;
+// console.log(datag.toString())
+
+// var datag = 12;
+// var datag = 1112.342;
+var datag = 10;
+
+
+// console.log(datag.toString());
+// console.log(datag.toLocaleString('en-in'));
+// console.log(datag.valueOf(3));
+// console.log(datag.toExponential(1))
+
+
+
+
+
+
 
 
 
