8177664d388df75cf83e7754ad76ceb980470d8a
sanujitmajhi <80968929+sanu2002@users.noreply.github.com>
2023-08-05 09:43:32 +0000

    this is all about date time we will learn more l..

--- .devcontainer/sanu/datetime.js
+++ .devcontainer/sanu/datetime.js
@@ -1,10 +1,23 @@
-console.log(Date)
-console.log(Date.UTC())
+// console.log(Date)
+// console.log(Date.UTC())
 
-let mydate=new Date('2012-12-01');
-console.log(mydate.toString())
+// let mydate=new Date('2012-12-01');
+// console.log(mydate.toString())
+// let localtime=new Date();
 
-console.log(`the current time is ${mydate}`)
+
+
+
+// console.log(localtime.toString())
+// console.log(localtime.toISOString())
+// console.log(localtime.toJSON())
+// console.log(localtime.toLocaleDateString())
+// console.log(localtime.toLocaleDateString().toLocaleUpperCase())
+
+
+
+
+// console.log(`the current time is ${mydate}`)
 
 
 // console.log(typeof Date.UTC)
@@ -12,4 +25,32 @@ console.log(`the current time is ${mydate}`)
 
 
 // date is object not a function just like Math theree is some proposal regarding this
+
+//  let a=Date();
+//  let b = new Date();
+//  console.log(typeof a)
+//  console.log(typeof b);
+// console.log(b.getDate())  here we will get the get the method bt using the object but in the first declaration its acts as a string so there is only string function showing on thta
+
+// // here we can change the style of the time function how do you want to print all these  things 
+
+// let data = b.toLocaleString('default',{
+//     weekday:'short',
+//     dayPeriod:'long',
+//     year:'numeric'
+
+
+// })
+
+// console.log(data);
+
+const time1 = 150000; 
+const time = 2; 
+
+
+// console.log(time/1000/60);
+
+console.log(time/60/);
+// console.log(Math.floor(time*60*3600*(10**3)));
+// console.log(Math.floor(time*60*3600*(10**3)));
 
