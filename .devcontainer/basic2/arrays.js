ddc44a1352a3c145115a8aae695732e6c3cc2307
sanujitmajhi <80968929+sanu2002@users.noreply.github.com>
2023-08-05 10:49:02 +0000

    array first commit

---
+++ .devcontainer/basic2/arrays.js
@@ -0,0 +1,75 @@
+// // let arrays=[1,2,4,5,6,7,8]
+// // // console.log(arrays.concat(5))
+// // // console.log(arrays.entries())
+// // // console.log(arrays.entries())#
+// // console.log(arrays);
+// // console.log(arrays.slice(1,3));
+// // console.log(arrays.splice(1,3));
+
+// // Interview question is there any diffrence between slice and splice
+// // ans- Yes there is a big difference between slice and splice for verfying this you need to excute each step wen
+// // you are priting the original array is 
+
+// // let original=new Array('A',1,2,3,4,5,6)
+// // console.log('A',original.slice(0,3));
+// // console.log(original)
+
+
+
+
+// // console.log('B',original.splice(0,3));
+
+// // console.log(original)
+
+
+// // unshift and shift
+
+// // let a=[1,2,3,4,5,6]
+
+// // console.log(a.unshift(1));
+// // console.log(a);
+
+// // console.log(a.join(2));
+// // this will convert into string and also join thr whole array at a time
+
+
+// // console.log(a.shift(4));
+// // console.log(a);
+
+
+
+
+
+// // WHAT IS SHALLOW COPY AND WHAT IS DEEEP CPIES IN JS ARRAY??
+
+// // _______________________________________________________
+
+// // let data={
+// //     name:'sanu',
+// //     place:'angul' 
+    
+// // }
+
+// // let data2=data;
+
+// // data2.name='salu'
+// // console.log(data)
+// // console.log(data2)
+
+// // let data=[1,2,4,5,6,8]
+
+// // let c=data;
+// // data[0]=1000
+// // console.log(c)
+// // console.log(data);
+
+// // for understing these concerpt you need toi build a grasp knowledge about the satck and heap memeory 
+
+// let a='sanu'
+
+// let b=a;
+
+// a='salu'
+// console.log(a)
+// console.log(b)
+
