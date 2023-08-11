ea8b05c74c50044223a4bda3026b368ecff8e14a
sanujitmajhi <80968929+sanu2002@users.noreply.github.com>
2023-08-11 10:32:26 +0000

    this is the part1 of function

---
+++ .devcontainer/function/01_function.js
@@ -0,0 +1,77 @@
+// console.log('hii my name is sanu');
+// console.log('hii my name is sanu');
+// console.log('hii my name is sanu');
+// console.log('hii my name is sanu');
+// console.log('hii my name is sanu');
+
+// let a=function(){
+//     console.log('hii my name is sanu');
+//     console.log('hii my name is sanu');
+//     console.log('hii my name is sanu');
+//     console.log('hii my name is sanu');
+//     console.log('hii my name is sanu');
+
+
+// }
+// a()
+
+// let sum=function(a,b){
+//     // console.log(a+b)
+//     // 2nd way and here you are giving out the value for outside of the scope
+//     // let s=a+b
+//     // return s
+//     return a+b
+
+// }
+
+// let d=sum(10,20)
+// // because we are not returning anything
+// console.log(d)
+
+
+// let logged_in=function(username='unknown'){
+//     if(username===undefined){
+//         console.log('please enter a name')
+
+//     }
+
+//     return `${username} logged in succesfully`
+
+// // }
+// let logged_in=function(username){
+//     if(username===undefined){
+//         console.log('please enter a name')
+        
+//         }
+        
+
+//     }
+
+
+
+
+
+// // let a=logged_in('sanu')
+// // let a=logged_in('') return the space 
+// let a=logged_in()   //for this type of conditioon we must use a consdition we will see it later but see some small code how you can handle it 
+
+
+// //you can either use default value or you can make some condition here
+
+
+
+
+
+
+
+
+// console.log(a)
+
+
+// we will discuss more about function in the second part
+// how sccope work how we solve complex calculation in 
+// function 
+// 1=this
+// 2=function object 
+// 3=global and local scope 
+// 4=function with object and array
