a6a703a2685003d7b49989266c6900f3ada097ab
sanujitmajhi <80968929+sanu2002@users.noreply.github.com>
2023-08-14 10:14:01 +0000

    this is all about functio

---
+++ .devcontainer/function/02_functions.js
@@ -0,0 +1,30 @@
+// rest operator and speread operator both are same and dependes upon use case 
+
+// const calcualteprice=function(...a){
+//             return a 
+// }
+
+// console.log(calcualteprice(1,2,3,4))
+
+
+// const calcualteprice=function(a,b,...c){
+//     return c
+// }
+
+// console.log(calcualteprice(200,400,500,2000))
+
+
+// const user={
+//     name:'sanu',
+//     price:199
+// }
+
+// function handleobject(user){
+//     return `username is ${user.name} and price is ${user.price}`
+// }
+
+// console.log(handleobject(user));
+
+
+
+            //   scope in 
