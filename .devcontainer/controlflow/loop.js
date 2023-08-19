8851d0fd3c97774bfc35aacee650660b360e2ba7
sanujitmajhi <80968929+sanu2002@users.noreply.github.com>
2023-08-19 12:07:48 +0000

    this is all about control flow

---
+++ .devcontainer/controlflow/loop.js
@@ -0,0 +1,149 @@
+//if
+// operator-:
+// <,>,>=,<=,==,!=,===(strict check),!==
+
+// if (true){
+
+// }
+// if false{
+
+// // }
+// // if (2=='2'){
+// //     console.log('excuted');
+
+// // }
+
+
+// const temp=41
+
+// if (temp<50){
+//     console.log('temp less than 50');
+// }else{
+//     console.log('temp greater than 50');
+    
+// }
+
+
+// let balance=50
+
+// // if (balance>49) console.log('yes'),console.log('aaaaa');
+
+// // but it is unredable 
+
+
+// if (true){
+
+// }
+// else if(false){
+
+// }
+// else{
+
+// }
+
+// const userloggedin=true
+// const debitcard=true
+
+// these are the flag value 
+
+// if (userloggedin && debitcard && 2==3){
+//     console.log('yes you can ');
+// }
+
+// let googleloggedin=true
+// let emailloggedin=true
+// let logeedinumber=true
+
+// // here we can work with user data wen we will go to ReadableByteStreamController.js 
+
+// if (googleloggedin==false && emailloggedin==false){
+//     console.log('you are succes]fullty soigned in ');
+// }
+
+
+// else{
+//     console.log('promt user to logged in through both');
+// }
+
+
+// soppoese you have to check the month and reeturn beack the correct month if the user type 1 then give january just like that 
+
+// let month=2
+// switch (month) {
+//     case 1:
+//         console.log('january');
+
+//     case 2:
+//         console.log('feb');
+
+        
+        
+//     case 3:
+//         console.log('march');
+        
+//         break;
+
+//     default:
+//         console.log('defaukt match');
+//         break;
+// }
+// so there is some check you have to keep in mind that these are the old school method construction which use this type of things in market 
+
+// lets talk about falssy and truthy key word 
+
+// let email={} truthy
+// let email=0 falsy
+// let email=[] truthy
+// let email='' falsy
+// let email=' ' if you give some space then it will be  a truthy value 
+// let email='0'   truthy
+// let email=undefined falsy 
+// let email=something inside the string is comsidered as truthy
+
+// let email=function(){}
+// let email=[]
+// let email={}
+
+
+// if (email.length===0){
+//     console.log('you are successfully logged in ');
+// }
+// else{
+//     console.log('plz give the correct');
+// }
+
+// if (Object.keys(email).length===0){
+//     console.log('you are successfully logged in ');
+// }
+// else{
+//     console.log('plz give the correct');
+// }
+
+// ternnary operator 
+
+// nullish collition operator
+
+// let a=null ?? 4;
+// console.log(a);
+
+
+// let a=undefined ?? 4;
+// console.log(a);
+
+
+// let a=null ?? undefined;
+// console.log(a);
+// why we need to use this ??
+// for safety use wen you are working in database or from the api 
+
+// let a=null ?? undefined ??2??4
+// console.log(a);
+
+
+
+// terniary operator 
+
+// condition ? true :false
+
+// let a=70
+// a>60 ? console.log('yes'):console.log('no');;
