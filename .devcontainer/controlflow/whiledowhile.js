45179dee51a7b73a310ada7a82471c2a02a60c42
sanujitmajhi <80968929+sanu2002@users.noreply.github.com>
2023-08-21 15:09:05 +0000

    whiledowhile

---
+++ .devcontainer/controlflow/whiledowhile.js
@@ -0,0 +1,20 @@
+let index=0
+while (index<=10){
+    console.log(`value of index is${index}`)
+    index+=2
+}
+
+myarray=['batman','superman','spiderman','ironman']
+let arr=0
+while (arr<myarray.length){
+    // console.log(myarray[arr]);
+    arr=arr+1
+
+}
+
+// do-while loop
+let score=1
+do{
+    console.log(`score is ${score}`);
+    score++
+}while(score<=10);
