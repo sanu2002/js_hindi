190df88df9537e66e1c10caa7de56ece3c5dfac4
sanujitmajhi <80968929+sanu2002@users.noreply.github.com>
2023-09-23 17:42:06 +0530

    Add files via upload
    This is my first project on js

--- .devcontainer/data.js
+++ .devcontainer/data.js
@@ -1,37 +1,120 @@
-// let a=20
-// let b='20'
-// console.log(typeof b)
-
-// let c=Number(b)
-// console.log(typeof c)
-
-// console.log(a+Number(b))
-
-// first you need to knwo the basic diffrence between let,var,const
+// we are going to discuss about all the necesaary concept with example so lets go  everybody we are on full mode 
 
 
-// let d=20
+// like you seee in other programming we are using the container varible for storing  datatypes so in  js we  will use let,const,var for storing the value of datatypes and why we  need to store because for holding the value for the future refrence we  need to store them  so  we  introduce let,var,const
+// lets discus about these thing how we can plat with the variabke
 
 
 
-// let diffrence
+// console.log(console)
+// console.warn('sanu')
+// console.dir('sanu')
+// console.timeEnd('sanu')
+// console.trace('sanu')
 
-// var a=20
-// var a=30
+
+// // Object [console] {
+// //     log: [Function: log],
+// //     warn: [Function: warn],
+// //     dir: [Function: dir],
+// //     time: [Function: time],
+// //     timeEnd: [Function: timeEnd],
+// //     timeLog: [Function: timeLog],
+// //     trace: [Function: trace],
+// //     assert: [Function: assert],
+// //     clear: [Function: clear],
+// //     count: [Function: count],
+// //     countReset: [Function: countReset],
+// //     group: [Function: group],
+// //     groupEnd: [Function: groupEnd],
+// //     table: [Function: table],
+// //     debug: [Function: debug],
+// //     info: [Function: info],
+// //     dirxml: [Function: dirxml],
+// //     error: [Function: error],
+// //     groupCollapsed: [Function: groupCollapsed],
+// //     Console: [Function: Console],
+// //     profile: [Function: profile],
+// //     profileEnd: [Function: profileEnd],
+// //     timeStamp: [Function: timeStamp],
+// //     context: [Function: context],
+// //     createTask: [Function: createTask]
+// //  
+
+
+
+// let a=23
+// let b=34
+// var c=34
+// var d=36
+// console.log(a,b,c,d)#
+
+
+
+// let a=23
+
+// {
+//     let a=25
+//     console.log(a)
+
+// }
 // console.log(a)
 
-// let a =20
-let b=[1,2,3,4,5,6,7]
-console.log('before',b)
+// // datatypes -> primitive,non-primitive datatypes 
+// primitive-> String,Number,null,undefined,Boolean,Symbol,BigInt
+// non-primitive-> Object,Array
 
-b[0]=3
-console.log(b)
+// before thta lets take a  look at ecma script what is that why we need to read some article about the ecma  script 
+// ij how many ways we can  write  and test our js code 
+// we can wrire it in 3 ways - with <script></script> and in browser console and in node environment where we are 
+// creating  a file extention with .js which give the instrction to execute that code 
+// before deep dive dive into lets have a deeep dive into datatypes because you know that datatypes are the most importatnt topic for 
+// every programmmer to get into that so lets go 
+
+// lets talk about primitive datatypes
 
 
-var a=30
-console.log(a)
-a=40
-console.log(a)
-// console.log('after',b);
+// 23 ? '32'
 
+
+// some operator in js 
+
+
+// ===
+// ==
+// >=
+// <=
+// *
+// +
+// + (addition)
+// - (subtraction)
+// * (multiplication)
+// / (division)
+// % (modulo, remainder)
+// ++ (increment)
+// -- (decrement)
+
+// let a =23
+
+
+// console.log(a++)
+// console.log(--a)
+// console.log(a++)
+
+
+// let a=null 
+// let a=undefined
+// let a=true
+// let c=false
+// console.log(a+c)
+
+// let a =Symbol(12)
+// console.log( typeof a)
+
+// type conversion ?
+
+
+// suppose u are extracting some value from web browswer what is that we are settingg to things here 1st one is 
+// we will take 2 input a and b and in tge backgroud i will make a calculation to show the output to the user for thta we
+// need to mastery about the function and type conversion and  DOM 
 
