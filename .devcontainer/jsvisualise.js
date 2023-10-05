9c4b29989f6a67b7a62b5016badd0222f9779656
sanujitmajhi <80968929+sanu2002@users.noreply.github.com>
2023-10-05 23:06:20 +0530

    Add files via upload
    this about my practice of event loop and hoisting

---
+++ .devcontainer/jsvisualise.js
@@ -0,0 +1,42 @@
+// function greet(){
+//     console.log("Hello, World!"); 
+// }
+
+
+// function option(){
+//     return setTimeout(()=>{
+//         return 'hey!' 
+//     },1000)
+// }
+
+
+
+// greet()
+// console.log(option())
+
+// name()
+// sanu()
+// function name(){
+//     console.log('i am sanu')
+// }
+
+
+// function sanu(){
+//     console.log('i am 2')
+// }
+
+// console.log(name)
+// let name='sanu'
+
+
+// console.log(sum(10,12))
+// function sum(x,y){
+//     return x+y
+//     }
+
+
+
+
+
+
+
